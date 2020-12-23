a1649cb7db43c8cc4cdd0d3e27122ca59b7c4bad
SealTok <74577246+SealTok@users.noreply.github.com>
2020-12-23 22:32:32 +0900

    Update TapeEquilibrium.py

--- TapeEquilibrium.py
+++ TapeEquilibrium.py
@@ -1,11 +1,31 @@
 def solution(A):
+    # A = [-1000,1000]
+    s = 0
+    p = 0
+    t = 0
+    m=2000
+    for a in A:
+        s+=a
+    for i in range(len(A)-1):
+        p+=A[i]
+        t = abs(s-p-p)
+        # print t
+        if t<m:
+            m = t
+    return m
 
-    Total = 0
-    Min = 100000
-    P1 = 0
+ #Task Score 100%   Correctness 100%  Performance 100%
+    
+    
+    
+def solution(A):
+
+    Total = 0    // s
+    Min = 100000  //m
+    P1 = 0        //p
     for a in A:
         Total = Total + a
-    for i in range(len(A)):
+    for i in range(len(A)):    //mistake!!
         P1 = P1+ A[i]
         P2 = Total-P1
         Diff = abs(P2 - P1)
